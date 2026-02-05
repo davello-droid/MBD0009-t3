a05196d88cc14c816c655721b5922235b888e6d7
davello-droid <danyavello@gmail.com>
2026-02-05 18:38:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import Pregunta_03
 import Pregunta_04
 import Pregunta_05
 import Pregunta_06
-#import Pregunta_07
+import Pregunta_07
 
 def main() -> None:
     Pregunta_01.run()
@@ -15,6 +15,7 @@ def main() -> None:
     Pregunta_04.run()
     Pregunta_05.run()
     Pregunta_06.run()
-    #Pregunta_07.run()
+    Pregunta_07.run()
 if __name__ == "__main__":
+
     main()
